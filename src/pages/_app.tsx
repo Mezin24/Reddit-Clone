900c2557cef69c90ec9020a31f3b66b2d4cdf5ef
Pavel Mezencev <mezencev24@mail.ru>
2023-05-03 00:06:21 +0300

    initial

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from 'next/app';
+import * as React from 'react';
+
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '../chakra/theme';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+import { NextPage } from 'next';
+
+const HomePage: NextPage = () => {
+  return <div>HomePage</div>;
+};
+export default HomePage;
